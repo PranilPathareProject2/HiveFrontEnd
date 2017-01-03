376871b9871712e6939caca317083762ba2f51fb
PranilPathareProject2 <pranilpathare93@gmail.com>
2017-01-04 00:27:01 +0530

    First Project

--- WebContent/Home/js/HomeController.js
+++ WebContent/Home/js/HomeController.js
@@ -1,4 +1,8 @@
-app.controller("HomeController", function($scope) {
+app.controller("HomeController", function($scope, $http) {
 	console.log("In home cotroller");
 	$scope.message="This is from home controller";
+	
+	$http.get('http://localhost:8081/HiveBackEnd/listusers').then(function (response){
+		$scope.users = response.data;
+	});
 });
