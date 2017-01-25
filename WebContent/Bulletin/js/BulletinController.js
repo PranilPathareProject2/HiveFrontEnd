ea0d7718e080a45b08b9b5aa9726f75860125bc2
PranilPathareProject2 <pranilpathare93@gmail.com>
2017-01-26 01:19:58 +0530

    Second Project

---
+++ WebContent/Bulletin/js/BulletinController.js
@@ -0,0 +1,143 @@
+app.controller('BulletinController', [ '$scope', 'BulletinService', '$rootScope', '$http', '$location', '$cookies', function($scope, BulletinService, $rootScope, $http, $location, $cookies){
+	var self = this;
+	self.bulletin = {"bulletin_id":"", "bulletin_title":"", "bulletin_description":"", "status":"", "errorCode":"", "errorMessage":""};
+	$rootScope.bulletins = [];
+	self.allbulletins = [];
+	
+	self.allBulletinsForAdmin = function() {
+		console.log("allBulletinsForAdmin method in controller started");
+		BulletinService
+			.allBulletinsForAdmin()
+			.then(
+					function(bldata) {
+						if(bldata[0].errorMessage != undefined)	
+						{
+							alert(bldata[0].errorMessage);
+							$location.path("/managebulletins");
+							delete $rootScope.bulletins;
+						}	
+						else
+						{	
+							$rootScope.bulletins = bldata;
+							$location.path("/managebulletins");
+						}	
+					},
+					function(errorresponse) {
+						console.error("Error while fetching bulletins");
+					}
+			);
+	};
+	
+	self.allBulletins = function() {
+		console.log("allBulletins method in controller started");
+		BulletinService
+			.allBulletins()
+			.then(
+					function(bldata) {
+						$rootScope.allbulletins = bldata;
+						$location.path("/viewbulletins");	
+					},
+					function(errorresponse) {
+						console.error("Error while fetching bulletins");
+					}
+			);
+	};
+		
+	self.addBulletin = function(bulletin) {
+		console.log("addBulletin method in controller started");
+		BulletinService
+			.addBulletin(bulletin)
+			.then(
+					function(fdata) {
+						alert(fdata.errorMessage);
+					},
+					function(errorresponse) {
+						console.error("Error while adding bulletin");
+					}
+			);
+	};
+	
+	self.activateBulletin = function(bulletin_id) {
+		console.log("activateBulletin method in controller started");
+		BulletinService
+			.activateBulletin(bulletin_id)
+			.then(
+					function(fdata) {
+						self.allBulletinsForAdmin();
+					},
+					function(errorresponse) {
+						console.error("Error while activating bulletin");
+					}
+			);
+	};
+	
+	self.deactivateBulletin = function(bulletin_id) {
+		console.log("deactivateBulletin method in controller started");
+		BulletinService
+			.deactivateBulletin(bulletin_id)
+			.then(
+					function(fdata) {
+						self.allBulletinsForAdmin();
+					},
+					function(errorresponse) {
+						console.error("Error while deactivating bulletin");
+					}
+			);
+	};
+	
+	self.getBulletin = function(bulletin_id) {
+		console.log("getBulletin method in controller started");
+		BulletinService
+			.getBulletin(bulletin_id)
+			.then(
+					function(bldata) {
+						
+						if(bldata.errorMessage != undefined)	
+						{
+							alert(bldata.errorMessage);
+						}	
+						else
+						{	
+							self.bulletin = bldata;
+						}
+					},
+					function(errorresponse) {
+						console.error("Error while fetching bulletin");
+					}
+			);
+	};
+
+	self.updateBulletin = function(bulletin) {
+		console.log("updateBulletin method in controller started");
+		BulletinService
+			.updateBulletin(bulletin)
+			.then(
+					function(bldata) {
+						alert(bldata.errorMessage);
+					},
+					function(errorresponse) {
+						console.error("Error while updating bulletin");
+					}
+			);
+	};
+	
+	self.submitBulletinFormToAdd = function() {
+		console.log("submitBulletinFormToAdd method in bulletin controller started");
+		self.addBulletin(self.bulletin);
+		self.resetbulletinform();
+		console.log("submitBulletinFormToAdd method in bulletin controller ended");
+	};
+	
+	self.submitBulletinFormToUpdate = function() {
+		console.log("submitBulletinFormToUpdate method in bulletin controller started");
+		self.updateBulletin(self.bulletin);
+		self.resetbulletinform();
+		console.log("submitBulletinFormToUpdate method in bulletin controller ended");
+	};
+	
+	self.resetbulletinform = function() {
+		console.log("reset method in bulletin controller started");
+		self.bulletin = {"bulletin_id":"", "bulletin_title":"", "bulletin_description":"", "status":"", "errorCode":"", "errorMessage":""};	
+	};
+	
+}]);
---
+++ WebContent/Bulletin/js/BulletinService.js
@@ -0,0 +1,71 @@
+'use strict';
+
+app.service('FriendService', [ '$http', '$q', '$rootScope', function($http, $q, $rootScope){
+	console.log("In FriendService.....");
+	var url = "http://localhost:8081/HiveBackEnd";
+	
+	return {
+		allBulletinsForAdmin: function() {
+			return $http.get(url+'/listbulletins').then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		allBulletins: function() {
+			return $http.get(url+'/activebulletins').then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		addBulletin: function(bulletin) {
+			return $http.post(url+'/addbulletin/'+bulletin).then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		activateBulletin: function(bulletin_id) {
+			return $http.put(url+'/activatebulletin/'+bulletin_id).then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		deactivateBulletin: function(bulletin_id) {
+			return $http.put(url+'/deactivatebulletin/'+bulletin_id).then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		getBulletin: function(bulletin_id) {
+			return $http.get(url+'/getbulletin/'+bulletin_id).then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		},
+		
+		updateBulletin: function(bulletin) {
+			return $http.put(url+'/updatebulletin/'+bulletin).then(
+				function(response){
+					return response.data;
+				},
+				null
+			);
+		}
+	};
+}]);
